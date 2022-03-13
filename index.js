a757ad4ffa11376c82639e4e23d6cfac88135c56
JRodriguez-Repo <97987743+JRodriguez-Repo@users.noreply.github.com>
2022-03-13 16:40:26 -0400

    updated password validation using regEx
    Signed-off-by: JRodriguez-Repo <97987743+JRodriguez-Repo@users.noreply.github.com>

--- index.js
+++ index.js
@@ -2,20 +2,29 @@ const form = document.getElementById('form');
 const pwd = document.getElementById('pwd');
 const pwdConfirm = document.getElementById('pwdConfirm');
 
-form.addEventListener('submit', ()=>{
 
+
+//Event listener to validate password entries using regEx
+
+form.addEventListener('submit', ()=> {
+
+    const regExPattern =  /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!?*&^$])[a-zA-Z0-9!?*&^$S]{8,15}/g;
    
-    if(pwd.length < 8 || pwd.length > 15 ){
+    if(pwd.length < 8 || pwd.length > 15 ) {
 
         alert('Password must be between 8 and 15 characters');
     }
-    if(pwd.value !== pwdConfirm.value){
+    if( pwdConfirm.value !== pwd.value ) {
 
         alert('Password entries must match');
     }
-    
+     if(pwd.value.match(regExPattern)) {
 
+       alert('Password accepted!');
+    }
+    else {
+        alert('Password must be a minimum of 8 characters long and contain one uppercase letter, one lowercase letter,' + 
+        'a digit, and one of the following special characters (!?*&^$)');
+    }
 
-    
-
-})
+});
